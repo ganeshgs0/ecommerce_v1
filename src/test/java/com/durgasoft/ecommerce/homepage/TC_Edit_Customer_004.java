ab840559945196690371d370108641ee9ed38ba1
ganeshgs0 <kanailalgs0@gmail.com>
2022-10-13 00:32:51 +0530

    added search_customer file and modified edit_customer file..

--- src/test/java/com/durgasoft/ecommerce/homepage/TC_Edit_Customer_004.java
+++ src/test/java/com/durgasoft/ecommerce/homepage/TC_Edit_Customer_004.java
@@ -1,2 +1,12 @@
 //this is a new test case
 // this test case wil check edit customer
+
+//this is the code added newly
+
+//this is the code added newly 
+
+//this is the code added newly 
+
+//this is the code added newly 
+
+//this is the code added newly 
---
+++ src/test/java/com/durgasoft/ecommerce/homepage/TC_Search_Customer.java
@@ -0,0 +1,7 @@
+package com.durgasoft.ecommerce.homepage;
+
+public class TC_Search_Customer {
+	
+	//this is search customer test case
+
+}
